89036711cf49e59234ea6e4c7e269ab3ff9dfa2f
Lyna05 <lyne_armelle@hotmail.com>
2024-10-16 21:12:29 +0200

    HA2-Adding the remote-exec-Provisioner.

--- terraform/modules/EC2_instance/main.tf
+++ terraform/modules/EC2_instance/main.tf
@@ -4,5 +4,20 @@ resource "aws_instance" "ec2_instance" {
   tags = {
     Name = var.instance_name
   }
-}
 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"  # oder der Benutzer, der für deine AMI-ID relevant ist
+      private_key = file("~/.ssh/your_private_key.pem")  # Pfad zu deinem SSH-Schlüssel
+      host        = self.public_ip
+    }
+  }
+}
