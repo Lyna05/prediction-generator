ef1923deb686851f042e28c6130900bfb064502d
Lyna05 <lyne_armelle@hotmail.com>
2024-10-16 12:02:05 +0200

    Adding my ID.

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -46,7 +46,7 @@ resource "aws_iam_policy" "lambda_dynamodb_policy" {
 				"dynamodb:Query",
 				"dynamodb:UpdateItem"
 			],
-			"Resource": "arn:aws:dynamodb:eu-central-1:273427624300:table/predictions"
+			"Resource": "arn:aws:dynamodb:eu-central-1:525313980360:table/predictions"
 		}
 	]
 })
