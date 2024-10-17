7e9e42d883badf213e2c55bb1d0b0d5892d0567b
Lyna05 <lyne_armelle@hotmail.com>
2024-10-17 10:28:38 +0200

    Updating the code of terraform dynamodb, iam, and lambda modules.

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,4 +1,34 @@
-output "lambda_function_arn" {
-    description = "The ARN of the Lambda function"
-    value = aws_lambda_function.lambda_function.arn
+variable "function_name" {
+    description = "The name of the Lambda function."
+    type = string
+}
+
+variable "lambda_role_arn" {
+    description = "The ARN of the Lambda execution role"
+    type = string
+}
+
+variable "handler" {
+    description = "The function handler in the code"
+    type = string
+}
+
+variable "runtime" {
+    description = "The runtime for the Lmabda function"
+    type = string
+}
+
+variable "lambda_zip_path" {
+    description = "The path to the Lambda ZIP file"
+    type = string
+}
+
+variable "lambda_environment_vars" {
+    description = "Environment variables for the Lambda function"
+    type = map(string)
+}
+
+variable "dynamodb_table_name" {
+  description = "The name of the DynamoDB table"
+  type        = string
 }
